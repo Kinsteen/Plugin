b9a4e18016dd41296bf0e4e5e2047f57c64d9d5a
pitiqui <pitiqui69@gmail.com>
2013-09-27 17:55:55 +0200

    Gros ajout: /health et /heal

--- All-In-One/AllInOne/Main.java
+++ All-In-One/AllInOne/Main.java
@@ -1,18 +1,24 @@
 package fr.pitiqui.AllInOne;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener
 {
+	Logger log = Bukkit.getLogger();
 	public void onEnable()
 	{
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        log.info("[All-In-One] Yeah you start All-In-One !");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
@@ -32,15 +38,40 @@ public class Main extends JavaPlugin implements Listener
             }
             
             String message = "";
-            for (int LOL = 1; LOL < args.length; LOL ++)	 
+            for (int i = 1; i < args.length; i ++)	 
             {
-            		message = message + args[LOL] + " ";
+            		message = message + args[i] + " ";
            	}
             String argument1 = args[0];
             p = Bukkit.getServer().getPlayer(argument1);
             p.chat(message);
             return true;
 		}
+		
+		if (cmd.getName().equalsIgnoreCase("health"))
+		{ 
+			int Hp;
+			Hp = Integer.parseInt(args[0]); 
+			p.setMaxHealth(Hp);
+			p.setHealth(Hp);
+			return true;
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("heal"))
+		{ 
+			p.setHealth(p.getMaxHealth());
+			return true;
+		}
 		return false;
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e)
+	{
+		log.info("[All-In-One] A player join ! Set health...");
+		int Hp = 40;
+		e.getPlayer().setMaxHealth(Hp);
+		e.getPlayer().setHealth(Hp);
+		log.info("[All-In-One] Yup ! That it's !");
 	}
 }
